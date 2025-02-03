be1b6cfc5a4bbdddda73aadaf12adf7f0a38a62d
sseezov <sseezov@mail.ru>
2025-02-03 14:14:56 +0700

    fix password

--- src/main/db.js
+++ src/main/db.js
@@ -3,7 +3,7 @@ import { Client } from 'pg';
 export default async () => {
   const client = new Client({
     user: 'postgres',
-    password: 'postgres',
+    password: '219219',
     host: 'localhost',
     port: '5432',
     database: 'demo_2025',
